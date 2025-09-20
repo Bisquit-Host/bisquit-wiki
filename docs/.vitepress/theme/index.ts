61500b0fac5922c33e49c2be1b0f16407baaec46
turbovadim <yolkin.vadim@gmail.com>
2025-09-20 18:08:02 +0300

    update ls guide

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,4 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
 import Layout from "./Layout.vue";
 import './style.css'
 import DefaultTheme from "vitepress/theme";

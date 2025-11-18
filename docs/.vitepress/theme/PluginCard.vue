2c4e3352edda2b2ba46231f61f33589aeb55e247
TopScrech <sergei_saliukov@icloud.com>
2025-11-18 13:56:52 +0100

    Revert "PluginCard's link images pushed to the right side"
    This reverts commit 3ab73a8123a6abdd11ce1eca35bb16061b3ffa8f.

--- docs/.vitepress/theme/PluginCard.vue
+++ docs/.vitepress/theme/PluginCard.vue
@@ -1,7 +1,5 @@
 <script setup>
-import { computed } from 'vue';
-
-const props = defineProps({
+defineProps({
   name: {
     type: String,
     required: true
@@ -27,33 +25,27 @@ const props = defineProps({
     default: ''
   }
 });
-
-const hasLinks = computed(() => props.bisquitLink || props.modrinthLink || props.bmLink || props.githubLink);
 </script>
 
 <template>
-  <div class="plugin-card">
-    <div class="plugin-card__header">
-      <div v-if="hasLinks" class="plugin-card__links">
-        <a v-if="bisquitLink" :href="bisquitLink">
-          <img src="/bisquit.webp" class="plugin-card__icon">
-        </a>
-        <a v-if="modrinthLink" :href="modrinthLink" target="_blank">
-          <img src="/modrinth.webp" class="plugin-card__icon sex">
-        </a>
-        <a v-if="bmLink" :href="bmLink" target="_blank">
-          <img src="/bm.webp" class="plugin-card__icon sex">
-        </a>
-        <a v-if="githubLink" :href="githubLink" target="_blank">
-          <img src="/github.webp" class="plugin-card__icon sex" :title="name">
-        </a>
-      </div>
-      <h3 class="plugin-card__title" title="Платный">
-        <b v-if="isPremium" style="color:lightgreen">₽</b> {{ name }}
-      </h3>
-    </div>
-    <keks>
-      <slot></slot>
-    </keks>
+  <div class="flex">
+    <h3 title="Платный">
+      <b v-if="isPremium" style="color:lightgreen">₽</b> {{ name }}
+    </h3>
+    <a v-if="bisquitLink" :href="bisquitLink">
+      <img src="/bisquit.webp">
+    </a>
+    <a v-if="modrinthLink" :href="modrinthLink" target="_blank">
+      <img src="/modrinth.webp" class="sex">
+    </a>
+    <a v-if="bmLink" :href="bmLink" target="_blank">
+      <img src="/bm.webp" class="sex">
+    </a>
+    <a v-if="githubLink" :href="githubLink" target="_blank">
+      <img src="/github.webp" class="sex" :title="name">
+    </a>
   </div>
+  <keks>
+    <slot></slot>
+  </keks>
 </template>
